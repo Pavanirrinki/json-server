c93866bfdf056bcd1b0bf0a860ffc3ffd8bf4bf4
unknown <pavankumarirrinki0123@gmail.com>
2023-06-16 08:29:24 +0530

    backend-data

---
+++ index.js
@@ -0,0 +1,8 @@
+const jsonserver = require("json-server")
+const server = jsonserver.create()
+const router =  jsonserver.router("db.json")
+const middlewares = jsonserver.defaults()
+const port = 8000
+server.use(middlewares);
+server.use(router);
+server.listen(port)
